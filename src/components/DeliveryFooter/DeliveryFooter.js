ba61c4b776e9765832b52c1ee54ff436416981ba
Youcef Bouchemat <adex@DESKTOP-10ARUFT.comparex.dz>
2022-09-30 13:18:36 +0100

    add map in delivery screen

---
+++ src/components/DeliveryFooter/DeliveryFooter.js
@@ -0,0 +1,43 @@
+import {View, Text, SafeAreaView, Image} from 'react-native';
+import React from 'react';
+import {
+  blackText,
+  greenColor,
+  greybgImage,
+  greyTextLight,
+  whiteColor,
+} from '../../../assets/colors';
+
+const DeliveryFooter = () => {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: whiteColor,
+        flexDirection: 'row',
+        padding: 16,
+        alignItems: 'center',
+      }}>
+      <Image
+        source={{
+          uri: 'https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450',
+        }}
+        style={{
+          height: 42,
+          width: 40,
+          padding: 5,
+          borderRadius: 50 / 2,
+          backgroundColor: greybgImage,
+        }}
+      />
+      <View style={{flex: 1, marginHorizontal: 20}}>
+        <Text style={{fontSize: 16, color: blackText}}>Sonny Sangha</Text>
+        <Text style={{fontSize: 14, color: greyTextLight}}>Your Rider</Text>
+      </View>
+      <Text style={{fontSize: 15, color: greenColor, fontWeight: 'bold'}}>
+        Call
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default DeliveryFooter;
--- src/screens/Delivery/Delivery.js
+++ src/screens/Delivery/Delivery.js
@@ -7,36 +7,59 @@ import {useSelector} from 'react-redux';
 import DeliveryTimeCard from '../../components/DeliveryTimeCard/DeliveryTimeCard';
 import selectRestaurant from '../../Redux/Reducers/restaurantSlice';
 import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
+import DeliveryFooter from '../../components/DeliveryFooter/DeliveryFooter';
+import {NavigationActions, StackActions} from 'react-navigation';
 const Delivery = () => {
   const navigation = useNavigation();
   //const restaurant = useSelector(selectRestaurant);
+  //const restaurant = useSelector(selectRestaurant);
   return (
     <View style={{flex: 1, backgroundColor: greenColor}}>
-      <SafeAreaView>
+      <SafeAreaView style={{zIndex: 1}}>
         <View
           style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
             padding: 20,
           }}>
-          <Icon name="close" type="antdesign" color={whiteColor} />
+          <Icon
+            name="close"
+            type="antdesign"
+            color={whiteColor}
+            onPress={() => {
+              navigation.reset({
+                index: 0,
+                routes: [{name: 'Home'}],
+              });
+            }}
+          />
           <Text style={{fontSize: 18, color: whiteTextColor}}>Order Help</Text>
         </View>
         <DeliveryTimeCard />
       </SafeAreaView>
       <MapView
         initialRegion={{
-          latitude: 36.74443325022872,
-          longitude: 3.0621307851058384,
+          latitude: 36.80191318456871,
+          longitude: 3.0461646468869312,
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
-        style={{elevation: 0, flex: 1, marginTop: -30}}
+        style={{flex: 1, marginTop: -50}}
+      />
+      <Marker
+        key={1}
+        coordinate={{
+          latitude: 36.80191318456871,
+          longitude: 3.0461646468869312,
+        }}
+        title={'hello'}
+        description={'hehehehehehehe hbcjahdjhdf dabdhadgb'}
+        identifier="origin"
+        pinColor={greenColor}
       />
 
-      <SafeAreaView style={{}}>
-        <Text>hello</Text>
-      </SafeAreaView>
+      <DeliveryFooter />
     </View>
   );
 };
